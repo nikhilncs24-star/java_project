9cb52abf50ebbdf1550cff9c75432dadf4a7bdce
Nikhil Raikar <nikhiln.cs24@bmsce.ac.in>
2025-12-23 00:07:57 +0530

    Update Food.java

--- HotelManagement/src/main/java/hotel/Food.java
+++ HotelManagement/src/main/java/hotel/Food.java
@@ -1,24 +1,24 @@
 package hotel;
 
 public class Food {
+    public enum Category {STARTER, MAIN, DESSERT, BEVERAGE}
+
     private String name;
     private double price;
+    private Category category;
 
-    public Food(String name, double price) {
+    public Food(String name, double price, Category category) {
         this.name = name;
         this.price = price;
+        this.category = category;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public Category getCategory() { return category; }
 
     @Override
     public String toString() {
-        return name + " - $" + price;
+        return name + " ($" + price + ") [" + category + "]";
     }
 }
