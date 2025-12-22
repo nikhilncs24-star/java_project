9db6dcf9b0124d604d0dfe3d5bcb41d09d116bf7
Nikhil Raikar <nikhiln.cs24@bmsce.ac.in>
2025-12-23 00:07:31 +0530

    Update Customer.java

--- HotelManagement/src/main/java/hotel/Customer.java
+++ HotelManagement/src/main/java/hotel/Customer.java
@@ -15,39 +15,25 @@ public class Customer {
         this.foodOrders = new ArrayList<>();
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public int getRoomNumber() { return roomNumber; }
+    public int getStayDays() { return stayDays; }
+    public ArrayList<Food> getFoodOrders() { return foodOrders; }
 
-    public int getRoomNumber() {
-        return roomNumber;
-    }
-
-    public int getStayDays() {
-        return stayDays;
-    }
-
-    public ArrayList<Food> getFoodOrders() {
-        return foodOrders;
-    }
-
-    public void addFood(Food food) {
-        foodOrders.add(food);
-    }
+    public void addFood(Food food) { foodOrders.add(food); }
 
     public double getFoodBill() {
         double total = 0;
-        for (Food f : foodOrders) {
-            total += f.getPrice();
-        }
+        for (Food f : foodOrders) total += f.getPrice();
         return total;
     }
 
-    public double getRoomBill(double pricePerDay) {
-        return stayDays * pricePerDay;
-    }
+    public double getRoomBill(double pricePerDay) { return stayDays * pricePerDay; }
 
-    public double getGrandTotal(double pricePerDay) {
-        return getRoomBill(pricePerDay) + getFoodBill();
+    public double getGrandTotal(double pricePerDay) { return getRoomBill(pricePerDay) + getFoodBill(); }
+
+    @Override
+    public String toString() {
+        return name + " | Room: " + roomNumber + " | Days: " + stayDays + " | Total: $" + getGrandTotal(0);
     }
 }
